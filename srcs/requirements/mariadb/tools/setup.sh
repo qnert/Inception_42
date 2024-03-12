381e99dbc8f5c37ef1ba713e884b1ea927b6e762
skunert <skunert@Inception.myguest.virtualbox.org>
2024-03-12 16:41:43 +0100

    added working mariadb with setup and the password of the env vars

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+source .env
+
 service mariadb start &
+
+sleep 5
+
+MYSQL_ROOT_PASSWORD=$(echo -n "$MYSQL_ROOT_PASSWORD" | sed -e 's/^[ \t]*//;s/[ \t]*$//')
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" &
 
 tail -f /dev/null
