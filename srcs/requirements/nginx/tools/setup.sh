eeeaba73c74b89dfc9b29fd28d5dd7f6b97c8b98
skunert <skunert@Inception.myguest.virtualbox.org>
2024-03-18 10:12:17 +0100

    added php config for php as well as ngnix config deeper understanding through experimenting with the config file and added working volumes with the help of youtube

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -9,15 +9,4 @@ chmod 700 $CERTS_
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_/key.key -out $CERTS_/certificate.crt \
       -subj "/C=DE/ST=BW/L=Heilbronn/O=42 Heilbronn/OU=IT Univeristy/CN=skunert.42.fr"
 
-echo "server{
-  listen 443 ssl;
-  server_name $DOMAIN;
-  index start.html;
-
-  ssl_certificate $CERTS_/certificate.crt;
-  ssl_certificate_key $CERTS_/key.key;
-  ssl_protocols TLSv1.3;
-
-}" > /etc/nginx/conf.d/main.conf
-
 nginx -g "daemon off;"
