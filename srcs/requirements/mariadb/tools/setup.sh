61dd23e2c8f1dd6aaf45c7c2522d87340f6a8105
skunert <skunert@Inception.myguest.virtualbox.org>
2024-03-21 15:10:02 +0100

    added all funcionalities for eval mandatory

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -2,14 +2,20 @@
 
 service mariadb start 2>/dev/null
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+if mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "USE $MYSQL_DATABASE;" &> /dev/null; then
+  kill $(cat /run/mysqld/mysqld.pid)
 
-mysql < db1.sql
+  mysqld
+else
+  echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+  echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+  echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
+  echo "FLUSH PRIVILEGES;" >> db1.sql
 
-kill $(cat /run/mysqld/mysqld.pid)
+  mysql < db1.sql
 
-mysqld
+  kill $(cat /run/mysqld/mysqld.pid)
+
+  mysqld
+fi
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+sleep 5
 
-wp core download --allow-root
+if ! command -v wp &> /dev/null; then
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+fi
 
-wp config create \
-    --dbname=$WORDPRESS_DB_NAME \
-    --dbuser=$WORDPRESS_DB_USER \
-    --dbpass=$WORDPRESS_DB_PASSWORD \
-    --dbhost=$WORDPRESS_DB_HOST \
-    --allow-root
-chmod 644 wp-config.php
-chmod 644 /etc/php/7.4/fpm/pool.d/www.conf
+if [ ! -f /var/www/html/wp-settings.php ]; then
+    wp core download --allow-root
+fi
 
-wp core install --url=$DOMAIN --title="Inception" \
-                 --admin_name=$WORDPRESS_ADMIN_USER \
-                 --admin_password=$WORDPRESS_ADMIN_PASSWORD \
-                 --admin_email=$WORDPRESS_ADMIN_EMAIL \
-                 --skip-email \
-                 --allow-root
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp config create \
+        --dbname=$WORDPRESS_DB_NAME \
+        --dbuser=$WORDPRESS_DB_USER \
+        --dbpass=$WORDPRESS_DB_PASSWORD \
+        --dbhost=$WORDPRESS_DB_HOST \
+        --allow-root
+    chmod 644 /var/www/html/wp-config.php
+fi
 
-wp user create $WORDPRESS_DB_USER $WORDPRESS_DB_USER_EMAIL \
-              --user_pass=$WORDPRESS_DB_PASSWORD \
-              --allow-root
+if ! wp core is-installed --allow-root; then
+    wp core install --url=$DOMAIN --title="Inception" \
+                     --admin_name=$WORDPRESS_ADMIN_USER \
+                     --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+                     --admin_email=$WORDPRESS_ADMIN_EMAIL \
+                     --skip-email \
+                     --allow-root
 
-mkdir /run/php
+    wp user create $WORDPRESS_DB_USER $WORDPRESS_DB_USER_EMAIL \
+                  --user_pass=$WORDPRESS_DB_PASSWORD \
+                  --allow-root
+fi
+
+if [ ! -d /run/php ]; then
+  mkdir -p /run/php
+fi
 
 php-fpm7.4 -F
